08114853edc3b57645ff168ed5f7a3e398134310
unknown <Administrator@.(none)>
2014-03-17 12:37:09 +0100

    zsxdcvbn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoolOglasnik::Application.config.secret_token = '34caf5bd7dd28f069b7b3926fc46084663007bdf900b8f3b18f7cd83b3ab2633b9f126d056b5dc0e135c9257eabaac8380a73fce2200e67ae6d012f45bb228f4'
