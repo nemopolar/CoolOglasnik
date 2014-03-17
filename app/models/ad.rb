77701d62f85bf52c7c95c57af02ab2a7eadf71aa
drjovaside <markohighroller@yahoo.com>
2014-03-17 21:19:25 +0100

    Uradjena prva activerecord veza- test

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,3 +1,4 @@
 class Ad < ActiveRecord::Base
   attr_accessible :description, :expiry_date, :price, :rating, :sold, :timestamp, :title, :user_id
+belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :adress, :banned, :city, :last_login, :last_name, :name, :password, :tel_num, :username
+has_many :ads
 end
